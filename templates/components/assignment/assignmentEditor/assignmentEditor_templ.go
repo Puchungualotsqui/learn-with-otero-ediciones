a4cc148718609db1c0e3457886f0bfe3fe06c38f
dabid <davidotero856@gmail.com>
2025-12-25 03:18:03 +1100

    professor bug side

--- templates/components/assignment/assignmentEditor/assignmentEditor_templ.go
+++ templates/components/assignment/assignmentEditor/assignmentEditor_templ.go
@@ -95,7 +95,7 @@ func AssignmentEditor(a *models.Assignment, classId int, firstRender bool) templ
 				return templ_7745c5c3_Err
 			}
 			var templ_7745c5c3_Var4 string
-			templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs("initExisting(" + filesJSON + "); originalDescription = `" + a.Description + "`; originalTitle = `" + a.Title + "`; originalDate = `" + a.DueDate + "`;")
+			templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs("originalDescription = `" + a.Description + "`; originalTitle = `" + a.Title + "`; originalDate = `" + a.DueDate + "`; initExisting(" + filesJSON + ");")
 			if templ_7745c5c3_Err != nil {
 				return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates/components/assignment/assignmentEditor/assignmentEditor.templ`, Line: 36, Col: 173}
 			}
